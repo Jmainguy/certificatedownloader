cd2431fa4d8602e8d21257abfc451b54a6579b48
Jonathan Mainguy <jon@soh.re>
2021-02-19 19:46:45 -0500

    Add support for https://
    Add support for --timeout
    
    Add friendlier error messages for timeout and updateFedora

--- main.go
+++ main.go
@@ -7,17 +7,28 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 // https://stackoverflow.com/a/46735876
-func getCertificatesPEM(address string) ([]byte, error) {
-	conn, err := tls.Dial("tcp", address, &tls.Config{
+func getCertificatesPEM(dialTimeout int, address string) ([]byte, error) {
+	dialer := &net.Dialer{
+		Timeout: time.Duration(dialTimeout) * time.Second,
+	}
+
+	conn, err := tls.DialWithDialer(dialer, "tcp", address, &tls.Config{
 		InsecureSkipVerify: true,
 	})
 	if err != nil {
+		if strings.Contains(err.Error(), "i/o timeout") {
+			fmt.Printf("Timed out connecting to %s, waited %d seconds\n", address, dialTimeout)
+			fmt.Println("if you are confident that you can connect, consider increasing the timeout with --timeout 30, for 30 seconds")
+			return []byte(""), err
+		}
 		return []byte(""), err
 	}
 	defer conn.Close()
@@ -39,6 +50,11 @@ func updateFedora(certs []byte, pemName string) {
 	path := "/etc/pki/ca-trust/source/anchors/" + pemName
 	err := ioutil.WriteFile(path, certs, 0644)
 	if err != nil {
+		if strings.Contains(err.Error(), "permission denied") {
+			fmt.Println(err)
+			fmt.Println("  Try running with sudo, sudo !!")
+			os.Exit(1)
+		}
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -53,24 +69,44 @@ func updateFedora(certs []byte, pemName string) {
 }
 
 func main() {
-	uriPtr := flag.String("uri", "jmainguy.com:443", "A hostname and port, jmainguy.com:443 for example")
+	uriPtr := flag.String("uri", "", "A hostname and port, jmainguy.com:443 for example")
 	pemPtr := flag.String("pem", "", "pem file to write to, insecure.pem by default")
 	updateFedoraPtr := flag.Bool("updateFedora", false, "write pem to /etc/pki/ca-trust/source/anchors and run update-ca-trust")
+	timeoutPtr := flag.Int("timeout", 10, "Timeout in seconds")
 
 	flag.Parse()
 
-	certs, err := getCertificatesPEM(*uriPtr)
+	if *uriPtr == "" {
+		flag.PrintDefaults()
+		fmt.Println("")
+		fmt.Println("  To save jmainguy.com:443 cert, run certificateDownloader --uri jmainguy.com:443 for example")
+		fmt.Println("")
+		os.Exit(1)
+	}
+
+	// Replace https:// and / with "", this will allow a host like https://jmainguy.com/ to be passed
+	// Even though, you really shouldnt be passing it in this format
+	uri := strings.ReplaceAll(*uriPtr, "https://", "")
+	uri = strings.ReplaceAll(uri, "/", "")
+
+	uriArray := strings.Split(uri, ":")
+	if len(uriArray) == 1 {
+		uri = uri + ":443"
+	} else if len(uriArray) > 2 {
+		fmt.Printf("Please format uri as host:port, you provided %s\n", uri)
+	}
+	certs, err := getCertificatesPEM(*timeoutPtr, uri)
 	var pemName string
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	if *pemPtr == "" {
-		uriSplit := strings.Split(*uriPtr, ":")
-		pemName = uriSplit[0] + "." + uriSplit[1] + ".pem"
+		pemName = strings.ReplaceAll(uri, ":", ".")
 	} else {
 		pemName = *pemPtr
 	}
+	pemName = pemName + ".pem"
 	if *updateFedoraPtr {
 		updateFedora(certs, pemName)
 	} else {
